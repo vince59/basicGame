5d0f2369493d0d3a2249f1ec442187c0896e2bf5
Vincent <vince.pno@gmail.com>
2025-07-06 17:52:06 +0200

    clamp

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,26 @@ use macroquad::prelude::*;
 async fn main() {
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
+    const MOVEMENT_SPEED: f32 = 200.0;
+    
     loop {
+        let delta_time = get_frame_time(); // temps passé depuis la dernière frame
         clear_background(BLUE);
         if is_key_down(KeyCode::Right) {
-            x += 1.0;
+            x += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Left) {
-            x -= 1.0;
+            x -= MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Down) {
-            y += 1.0;
+            y += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Up) {
-            y -= 1.0;
+            y -= MOVEMENT_SPEED * delta_time;
         }
+        // on s'assure qu'on ne déborde pas de l'écran
+        x = clamp(x, 0.0, screen_width());
+        y = clamp(y, 0.0, screen_height());
         draw_circle(x, y, 16.0, YELLOW);
         next_frame().await
     }
