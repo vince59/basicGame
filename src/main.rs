a6a084699c4f3fc60b7aaeaea4780d3cfaaf760e
Vincent <vince.pno@gmail.com>
2025-07-06 17:57:30 +0200

    speed

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use macroquad::prelude::*;
 async fn main() {
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
-    const MOVEMENT_SPEED: f32 = 200.0;
-    
+    const MOVEMENT_SPEED: f32 = 500.0;
+    const RADIUS: f32 = 16.0;
     loop {
         let delta_time = get_frame_time(); // temps passé depuis la dernière frame
         clear_background(BLUE);
@@ -21,9 +21,9 @@ async fn main() {
             y -= MOVEMENT_SPEED * delta_time;
         }
         // on s'assure qu'on ne déborde pas de l'écran
-        x = clamp(x, 0.0, screen_width());
-        y = clamp(y, 0.0, screen_height());
-        draw_circle(x, y, 16.0, YELLOW);
+        x = clamp(x, RADIUS, screen_width()-RADIUS);
+        y = clamp(y, RADIUS, screen_height()-RADIUS);
+        draw_circle(x, y, RADIUS, YELLOW);
         next_frame().await
     }
 }
