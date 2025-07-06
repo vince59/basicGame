4fe4c85c2426d4425add98a0e05521fad9c21c6c
Vincent <vince.pno@gmail.com>
2025-07-06 17:38:26 +0200

    draw

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # basicGame
+https://vince59.github.io/basicGame/
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use macroquad::prelude::*;
 #[macroquad::main("My game")]
 async fn main() {
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() / 2.0;
     loop {
         clear_background(BLUE);
+        if is_key_down(KeyCode::Right) {
+            x += 1.0;
+        }
+        if is_key_down(KeyCode::Left) {
+            x -= 1.0;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += 1.0;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= 1.0;
+        }
+        draw_circle(x, y, 16.0, YELLOW);
         next_frame().await
     }
 }
